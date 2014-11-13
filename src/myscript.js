0f1af09ca33155ad39c7dc4699e793d38e3aa75c
Piski <sergeiliski@gmail.com>
2014-11-13 02:45:35 +0200

    update #2
    added images with jquery + lightbox

--- src/myscript.js
+++ src/myscript.js
@@ -1,13 +1,27 @@
-if (typeof jQuery == 'undefined') {  
-    console.log("not working")
-} else {
-    console.log("working")
-}
-
-
 // varmista et sivu on latautunut
 $(document).ready(function() {
     console.log( "ready" );
-	$("#hetaImage").append('<img id="heta" width="50%" height="50%" src="assets/ace.jpg" />');
-	$("#SergeiImage").append('<img id="heta" src="ace.jpg" />');
+    
+    // hetan kuva
+    $("#hetaImage").append('<a href="assets/stockImageOne.jpg" data-lightbox="stockImageOne" data-title="Heta Pärssinen"><img id="heta" width="50%" height="50%" src="assets/stockImageOne.jpg" /></a>');
+    
+    $("#hetaImage").fadeIn(1000);
+    
+    $('#heta').hover(function() {
+        $('#heta').fadeTo(500, 0.5);
+    }, function() {
+        $('#heta').fadeTo(200, 1);
+    });
+    
+    // sergein kuva
+    //$("#sergeiImage").append('<img id="sergei" width="50%" height="50%" src="assets/stockImageTwo.png" />');
+    $("#sergeiImage").append('<a href="assets/stockImageTwo.png" data-lightbox="stockImageTwo" data-title="Sergei Liski"><img id="sergei" width="50%" height="50%" src="assets/stockImageTwo.png" /></a>');
+    $("#sergeiImage").fadeIn(1000);
+    
+    $('#sergei').hover(function() {
+        $('#sergei').fadeTo(500, 0.5);
+    }, function() {
+        $('#sergei').fadeTo(200, 1);
+    });
+    
 });
