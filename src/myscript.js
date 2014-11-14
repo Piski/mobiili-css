71b9afa636f27539d4185d612db0d7de66eec0d6
User <User>
2014-11-14 13:54:20 +0200

    new look

--- src/myscript.js
+++ src/myscript.js
@@ -1,6 +1,14 @@
 // varmista et sivu on latautunut
 $(document).ready(function() {
     console.log( "ready" );
+
+    var hetaText = "<ul id='hetaList'><li>Nimi: Heta Pärssinen</li><li>Puhelin: Samsung Ace</li></ul>";
+    var sergeiText = "<ul id='sergeiList'><li>Nimi: Sergei Liski</li><li>Puhelin: LG G2</li></ul>";
+
+    // add text
+    $("#hetaText").append(hetaText);
+
+    $("#sergeiText").append(sergeiText);
     
     // hetan kuva
     $("#hetaImage").append('<a href="assets/stockImageOne.jpg" data-lightbox="stockImageOne" data-title="Heta Pärssinen"><img id="heta" width="50%" height="50%" src="assets/stockImageOne.jpg" /></a>');
@@ -14,7 +22,6 @@ $(document).ready(function() {
     });
     
     // sergein kuva
-    //$("#sergeiImage").append('<img id="sergei" width="50%" height="50%" src="assets/stockImageTwo.png" />');
     $("#sergeiImage").append('<a href="assets/stockImageTwo.png" data-lightbox="stockImageTwo" data-title="Sergei Liski"><img id="sergei" width="50%" height="50%" src="assets/stockImageTwo.png" /></a>');
     $("#sergeiImage").fadeIn(1000);
     
